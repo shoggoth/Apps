a9c43018133e362014821c246bb2f1639db8210b
rich@wight <r.j.henry@gmail.com>
2023-05-03 14:05:14 +0100

    Going to look at accent colour

--- MindGuard/MindGuard/MainView.swift
+++ MindGuard/MindGuard/MainView.swift
@@ -8,18 +8,32 @@
 import SwiftUI
 
 struct MainView: View {
+    private enum Tag {
+        case map
+        case depsych
+        case chalk
+        case hitList
+        case psIdent
+    }
+    @State private var selectedTab = Tag.psIdent
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             HitMapView()
                 .tabItem { Label("HitMap", systemImage: "list.dash") }
+                .tag(Tag.map)
             DepsychView()
                 .tabItem { Label("Depsych", systemImage: "list.dash") }
+                .tag(Tag.depsych)
             ChalkView()
                 .tabItem { Label("PsyChalk", systemImage: "list.dash") }
+                .tag(Tag.chalk)
             HitListView()
                 .tabItem { Label("Hits", systemImage: "list.dash") }
+                .tag(Tag.hitList)
             PsIdentView()
                 .tabItem { Label("PsIdent", systemImage: "list.dash") }
+                .tag(Tag.psIdent)
         }
     }
 }
--- MindGuard/MindGuard/PsIdentView.swift
+++ MindGuard/MindGuard/PsIdentView.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 struct PsIdentView: View {
     var body: some View {
-        Text("Biorythm and Eponymlog go here.")
+        VStack(spacing: 23) {
+            Text("Biorythm and Eponymlog go here.")
+            Button("Vanilla Button") {
+                print("pressed")
+            }
+            Button("Capsule Button") {
+                print("pressed")
+            }
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .clipShape(Capsule())
+            Button("Capsule Button") {
+                print("pressed")
+            }
+            .buttonStyle(BlueButton())
+            Button("Capsule Button") {
+                print("pressed")
+            }
+            .buttonStyle(GrowingButton())
+        }
     }
 }
 
 struct PsIdentView_Previews: PreviewProvider {
     static var previews: some View {
         PsIdentView()
+    }
+}
+
+struct BlueButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .foregroundColor(.accentColor)
+            .clipShape(Capsule())
+    }
+}
+
+struct GrowingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .foregroundColor(.accentColor)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
