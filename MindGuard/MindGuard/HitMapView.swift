df93078ed915a4e910dfd65d68074ab2fdee0f20
rich@wight <r.j.henry@gmail.com>
2023-05-18 14:35:13 +0100

    Added Map package and a demo of the functioning thereof

--- MindGuard/MindGuard/HitMapView.swift
+++ MindGuard/MindGuard/HitMapView.swift
@@ -5,6 +5,7 @@
 //  Created by Richard Henry on 29/03/2023.
 //
 
+import Map
 import SwiftUI
 import MapKit
 
@@ -13,7 +14,13 @@ struct HitMapView: View {
 
     var body: some View {
         VStack {
-            Map(coordinateRegion: $region)
+            Map(coordinateRegion: $region, type: .standard)
+                .mapKey(1)
+                .overlay(alignment: .topLeading) {
+                    MapScale(key: 1, alignment: .leading, visibility: .visible)
+                        .fixedSize()
+                        .padding(12)
+                }
         }
         .ignoresSafeArea(.all, edges: [.top, .trailing, .leading])
     }
